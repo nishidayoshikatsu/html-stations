ef688f983f485e68eb72007a140cbd850c24850f
nishidayoshikatsu <nsd24technology@gmail.com>
2021-06-06 12:47:43 +0900

    JS 15 is complete

--- src/scripts/station15.js
+++ src/scripts/station15.js
@@ -6,11 +6,20 @@ async function getData() {
     { id: 1, first_name: '優', family_name: '大木', affilication: 'TechTrain', is_student: false },
     { id: 2, first_name: '太郎', family_name: '山田', affilication: 'HogeHoge大学', is_student: true }
   ];
-  const result = await test()
-  return await result
+  const result = await test(userList)
+  return result
 }
 
-function test() {
-  return
+function test(list) {
+  return new Promise(resolve => {
+    setTimeout(resolve, 3000, list.map(
+        l => {
+          l['full_name'] =l['family_name'] + ' ' + l['first_name']
+          return l
+        }
+    ))
+  }).catch(() => {
+    console.error('Something wrong!')
+  })
 }
 
